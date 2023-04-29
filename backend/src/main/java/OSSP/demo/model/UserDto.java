0da8cf9b480b1bcc26de8e736dbf1983ee1b446b
Hankyul Min <denirokr22@gmail.com>
2023-04-29 22:45:40 +0900

    [23.04.29, Hankyul] (Mod) UserDto 기본 Dto와 JoinRequestDto의 static inner class 구조로 변경하여 중복 제거, Custom Validation Enum 사용하도록 수정

--- backend/src/main/java/OSSP/demo/model/UserDto.java
+++ backend/src/main/java/OSSP/demo/model/UserDto.java
@@ -1,81 +1,61 @@
 package OSSP.demo.model;
 
-import OSSP.demo.entity.User;
+import OSSP.demo.entity.Dept;
+import OSSP.demo.entity.StudentId;
 import lombok.*;
-import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
 
-/**
- * request, response DTO 클래스를 하나로 묶어 InnerStaticClass로 한 번에 관리
- */
+
+//UserDto 내부에 UserJoinRequestDto를 Static 클래스로 두어 관리, UserJoinRequestDto는 회원가입 시 필요한 정보 검증
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+    private String token;
+    private String id;
+    private String studentId;
+    private String password;
+    private String name;
+    private String dept;
 
-    /** 회원 Service 요청(Request) DTO 클래스 */
     @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
     @Builder
-    public static class Request {
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserJoinRequestDto {
+        private Dept deptData;
+        private StudentId studentIdData;
 
-        @Range(min = 2016000000, max = 2023999999, message = "학번은 2016000000~2023999999 사이의 숫자여야 합니다.")
-        private int studentId;
 
-        @AssertTrue(message = "학과를 선택해주세요.")
-        public boolean isDeptValid() {
-            List<String> deptList = Arrays.asList(new String[]{"건설환경공학과", "멀티미디어공학과", "산업시스템공학과"});
-            return deptList.contains(dept);
-        }
+        private String id;
 
-        private String dept;
+        @NotBlank(message = "학번을 입력해주세요.")
+        private String studentId;
 
-        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
-        @NotBlank(message = "이름은 필수 입력 값입니다.")
-        private String name;
-
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String password;
 
-        public User toEntity() {
-            User user = User.builder()
-                    .student_Id(studentId)
-                    .dept(dept)
-                    .name(name)
-                    .password(password)
-                    .build();
-            return user;
+        @NotBlank(message = "이름을 입력해주세요.")
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
+        private String name;
+
+        @NotBlank(message = "학과를 선택해주세요.")
+        private String dept;
+
+        @AssertTrue(message = "학번이 등록되어있지 않습니다.")
+        private boolean isValidStudentId() {
+            int studentIdValue = Integer.parseInt(studentId);
+            return studentIdValue >= studentIdData.STUDENT_ID_MIN.getValue() && studentIdValue <= studentIdData.STUDENT_ID_MAX.getValue();
         }
 
-        /**
-         * 인증된 사용자 정보를 세션에 저장하기 위한 클래스
-         * 세션을 저장하기 위해 User 엔티티 클래스를 직접 사용하게 되면 직렬화를 해야 하는데,
-         * 엔티티 클래스에 직렬화를 넣어주면 추후에 다른 엔티티와 연관관계를 맺을시
-         * 직렬화 대상에 다른 엔티티까지 포함될 수 있어 성능 이슈 우려가 있기 때문에
-         * 세션 저장용 Dto 클래스 생성
-         * */
-        @Getter
-        public static class Response implements Serializable {
-
-            private final Long id;
-            private final Integer studentId;
-            private final String dept;
-            private final String name;
-            private final String password;
-
-            /* Entity -> dto */
-            public Response(User user) {
-                this.id = user.getUser_Id();
-                this.studentId = user.getStudent_Id();
-                this.dept = user.getDept();
-                this.name = user.getName();
-                this.password = user.getPassword();
-            }
+        @AssertTrue(message = "학과를 입력해주세요.")
+        public boolean isDeptValid() {
+            return deptData.getDeptList().contains(dept);
         }
-
     }
 }
