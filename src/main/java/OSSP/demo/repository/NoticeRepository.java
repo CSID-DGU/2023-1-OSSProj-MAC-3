4918e647ab7468d21463105516b9fc83d065e80b
thisishwan2 <feel000617@gmail.com>
2023-04-29 15:09:07 +0900

    [23.04.21, pil hwan] (Delete) unused file

--- src/main/java/OSSP/demo/repository/NoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package OSSP.demo.repository;
-
-import OSSP.demo.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
