682d1e86ae3ffe3fddc9b707515fb8525e7bb3dd
thisishwan2 <feel000617@gmail.com>
2023-04-29 17:50:07 +0900

    [23.04.21, pil hwan] (Add) create FileRepository

---
+++ backend/src/main/java/OSSP/demo/repository/FileRepository.java
@@ -0,0 +1,10 @@
+package OSSP.demo.repository;
+
+import OSSP.demo.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+
+}
