2bcc386ea1f897657b11357689333d10e0e806c4
Hawn <112103038+thisishwan2@users.noreply.github.com>
2023-04-29 15:06:37 +0900

    [23.04.29, pil hwan](Delete) unused file

--- src/main/java/OSSP/demo/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package OSSP.demo.repository;
-
-import OSSP.demo.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepository extends JpaRepository<File, Long> {
-}
