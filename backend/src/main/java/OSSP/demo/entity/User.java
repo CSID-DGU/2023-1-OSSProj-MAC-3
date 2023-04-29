988b7b05c7afdc49ff1981518877f35f64c9eff1
thisishwan2 <feel000617@gmail.com>
2023-04-29 15:07:32 +0900

    [23.04.21, pil hwan] (Add) User entity

---
+++ backend/src/main/java/OSSP/demo/entity/User.java
@@ -0,0 +1,40 @@
+package OSSP.demo.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 10, unique = true)
+    private int studentId;
+
+    @Column(nullable = false, length = 20, unique = false)
+    private String dept;
+
+    @Column(nullable = false, length = 10, unique = false)
+    private String name;
+
+    @Column(length = 50)
+    private String password;
+
+    @Builder
+    public User(String name, String password, String dept, int studentId){
+        this.name = name;
+        this.password = password;
+        this.dept = dept;
+        this.studentId = studentId;
+    }
+
+
+}
