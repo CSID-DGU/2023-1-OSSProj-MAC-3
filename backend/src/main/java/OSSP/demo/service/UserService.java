d2f8d0738008f360026200911a984560abf291f5
Hankyul Min <denirokr22@gmail.com>
2023-04-30 08:41:36 +0900

    [23.04.30, Hankyul] (Add) 게시판, TodoList 오픈소스 참고하여 UserService 로그인 기능 작성

--- backend/src/main/java/OSSP/demo/service/UserService.java
+++ backend/src/main/java/OSSP/demo/service/UserService.java
@@ -81,4 +81,42 @@ public class UserService {
         }
         return userRepository.save(user);
     }
+
+    public ResponseEntity<?> getResponseEntity(UserDto userDto) {
+        User user = authenticateStudentId(userDto.getStudentId());
+        if (user == null) {
+            String message = "학번이 올바르지 않습니다.";
+            ResponseDto responseErrorDto = getResponseErrorDto(message);
+            return ResponseEntity.badRequest().body(responseErrorDto);
+        }
+        if (!encoder.matches(userDto.getPassword(), user.getPassword())) {
+            String message = "비밀번호가 올바르지 않습니다.";
+            ResponseDto responseErrorDto = getResponseErrorDto(message);
+            return ResponseEntity.badRequest().body(responseErrorDto);
+        }
+        final UserDto responseUserDto = getResponseUserDto(userDto, user);
+        return ResponseEntity.ok().body(responseUserDto);
+    }
+
+    private static ResponseDto getResponseErrorDto(String message) {
+        Map<String, String> loginResult = new HashMap<>();
+        loginResult.put("valid_login", message);
+        ResponseDto responseDto = ResponseDto.builder().error(loginResult).build();
+        return responseDto;
+    }
+
+    private static UserDto getResponseUserDto(UserDto userDto, User user) {
+        final UserDto responseUserDto = userDto.builder()
+                .id(user.getId())
+                .studentId(user.getStudentId())
+                .name(user.getName())
+                .dept(user.getDept())
+                .build();
+        return responseUserDto;
+    }
+
+    public User authenticateStudentId(final String studentId) {
+        return userRepository.findByStudentId(studentId)
+                .orElse(null);
+    }
 }
