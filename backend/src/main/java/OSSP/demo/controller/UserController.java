61c731c32ae2ebc344159e3d7a4510bfd967e8f4
Hankyul Min <denirokr22@gmail.com>
2023-04-30 01:17:10 +0900

    [23.04.30, Hankyul] (Add) userController 회원가입 부분 구현

---
+++ backend/src/main/java/OSSP/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package OSSP.demo.controller;
+
+import OSSP.demo.model.ResponseDto;
+import OSSP.demo.model.UserDto;
+import OSSP.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserDto.UserJoinRequestDto userDto, Errors errors) {
+        if (errors.hasErrors()) {
+            ResponseDto responseDto = ResponseDto.builder().error(userService.validateHandling(errors)).build();
+            return ResponseEntity.badRequest().body(responseDto);
+        }
+        return userService.getResponseEntity(userDto);
+    }
+}
