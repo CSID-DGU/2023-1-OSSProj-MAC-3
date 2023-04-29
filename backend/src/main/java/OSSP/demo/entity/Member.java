fb0f4cc58933690cb1c25694d1bceb33ece3c79c
thisishwan2 <feel000617@gmail.com>
2023-04-29 17:49:20 +0900

    [23.04.21, pil hwan] (Add) Member entity

---
+++ backend/src/main/java/OSSP/demo/entity/Member.java
@@ -0,0 +1,32 @@
+package OSSP.demo.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "teamId")
+    private Team team;
+
+    @OneToMany(mappedBy = "member")
+    List<File> fileList = new ArrayList<>();
+
+
+}
