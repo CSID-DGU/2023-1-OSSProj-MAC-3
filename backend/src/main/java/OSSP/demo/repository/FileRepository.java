59afb7b54e95307180f67ac467a4251f951d4e06
thisishwan2 <feel000617@gmail.com>
2023-04-30 12:35:06 +0900

    [23.04.30, pil hwan] (Fix) findFileByFileName 메서드 추가

--- backend/src/main/java/OSSP/demo/repository/FileRepository.java
+++ backend/src/main/java/OSSP/demo/repository/FileRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
 
+    //파일이름(요청한 파일)으로 파일을 찾는 메서드.
+    File findFileByFileName(String fileName);
 }
