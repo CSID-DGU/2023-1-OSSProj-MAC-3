b30b3c73dd72ede244fb154a3d9b3595d0bbb454
thisishwan2 <feel000617@gmail.com>
2023-04-30 14:10:03 +0900

    [23.04.30, pil hwan](Fix) id를 Long으로 수정

--- backend/src/main/java/OSSP/demo/repository/UserRepository.java
+++ backend/src/main/java/OSSP/demo/repository/UserRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+<<<<<<< Updated upstream
 public interface UserRepository extends JpaRepository<User, Long> {
     /* Security */
+=======
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+>>>>>>> Stashed changes
     Optional<User> findByStudentId(String studentId);
 
     /* 중복 검사> 중복인 경우 true, 중복되지 않은경우 false 리턴 */
