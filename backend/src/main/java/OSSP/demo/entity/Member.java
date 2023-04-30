611a3aec13985ad54d0659cd3d761d6d3b68763c
thisishwan2 <feel000617@gmail.com>
2023-04-30 12:34:25 +0900

    [23.04.30, pil hwan] (Fix) Builder Pattern

--- backend/src/main/java/OSSP/demo/entity/Member.java
+++ backend/src/main/java/OSSP/demo/entity/Member.java
@@ -1,6 +1,7 @@
 package OSSP.demo.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,5 +29,26 @@ public class Member {
     @OneToMany(mappedBy = "member")
     List<File> fileList = new ArrayList<>();
 
+    @Builder
+    public Member (Long memberId, User user, Team team){
+        this.memberId = memberId;
+        this.user = user;
+        this.team = team;
+    }
 
+    // ==연관관계 편의 메서드==
+    public void setUser(User user){
+        this.user=user;
+        user.getMembers().add(this);
+    }
+
+    public void setTeam(Team team){
+        this.team=team;
+        user.getMembers().add(this);
+    }
+
+    public void addFileItem(File file){
+        fileList.add(file);
+        file.setMember(this);
+    }
 }
