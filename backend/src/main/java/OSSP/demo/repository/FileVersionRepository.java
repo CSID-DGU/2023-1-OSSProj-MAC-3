2bdddad421a2d4e311953695833d4a19ab97bffd
thisishwan2 <feel000617@gmail.com>
2023-05-03 16:07:53 +0900

    [23.05.03, pil hwan](Add) FileVersionRepository 메서드 생성

--- backend/src/main/java/OSSP/demo/repository/FileVersionRepository.java
+++ backend/src/main/java/OSSP/demo/repository/FileVersionRepository.java
@@ -4,4 +4,6 @@ import OSSP.demo.entity.FileVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileVersionRepository extends JpaRepository<FileVersion, Long> {
+
+    FileVersion findByFileVersionIdAndFile_FileId(Long fileId, Long fileVersionId);
 }
