dd85bbd9868b527978dc31264e7589e72c90b025
thisishwan2 <feel000617@gmail.com>
2023-05-03 16:08:11 +0900

    [23.05.03, pil hwan](Add) FileDownloadService 생성

---
+++ backend/src/main/java/OSSP/demo/service/download/FileDownloadService.java
@@ -0,0 +1,23 @@
+package OSSP.demo.service.download;
+
+import OSSP.demo.entity.FileVersion;
+import OSSP.demo.repository.FileRepository;
+import OSSP.demo.repository.FileVersionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class FileDownloadService {
+
+    private final FileVersionRepository fileVersionRepository;
+
+    @Transactional
+    public String downloadImage(Long fileId, Long fileVersionId) {
+        //findby조건을 달때, 해당 레포지토리의 pk부터 달아야함.
+        FileVersion findFile = fileVersionRepository.findByFileVersionIdAndFile_FileId(fileVersionId, fileId);
+        return findFile.getS3Url();
+    }
+}
