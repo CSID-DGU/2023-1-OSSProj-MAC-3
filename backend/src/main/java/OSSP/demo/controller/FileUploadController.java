c5f2b3177bcd678e70069564d7a326437b86265e
thisishwan2 <feel000617@gmail.com>
2023-05-03 16:05:18 +0900

    [23.05.03, pil hwan](Fix) FileUploadController 로직수정

--- backend/src/main/java/OSSP/demo/controller/FileUploadController.java
+++ backend/src/main/java/OSSP/demo/controller/FileUploadController.java
@@ -1,13 +1,9 @@
 package OSSP.demo.controller;
 
-
-import OSSP.demo.model.FileInfoReq;
+import OSSP.demo.model.FileVersionDto;
 import OSSP.demo.service.upload.FileUploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
@@ -17,7 +13,7 @@ public class FileUploadController {
     private final FileUploadService fileUploadService;
 
     @PostMapping("/{memberId}/upload")
-    public String uploadFile(@PathVariable Long memberId, @RequestPart MultipartFile file, FileInfoReq fileInfoReq){
-        return fileUploadService.uploadImage(memberId, file, fileInfoReq);
+    public String uploadFile(@PathVariable Long memberId, @RequestPart MultipartFile file, @RequestPart FileVersionDto fileVersionDto){
+        return fileUploadService.uploadImage(memberId, file, fileVersionDto);
     }
 }
