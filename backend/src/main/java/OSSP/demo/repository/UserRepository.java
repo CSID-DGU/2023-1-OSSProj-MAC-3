41ee6534688c3ed8d49f8189d072d59c339aab32
Hankyul Min <denirokr22@gmail.com>
2023-05-07 10:16:10 +0900

    [23.05.07, Hankyul] (Mod) UserRepository에 @Repository 어노테이션 추가

--- backend/src/main/java/OSSP/demo/repository/UserRepository.java
+++ backend/src/main/java/OSSP/demo/repository/UserRepository.java
@@ -2,9 +2,11 @@ package OSSP.demo.repository;
 
 import OSSP.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     /* Security */
 
