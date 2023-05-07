5d6a9c5393ba07bbc6326c956148d1b946eff331
Hankyul Min <denirokr22@gmail.com>
2023-05-07 10:22:45 +0900

    [23.05.07, Hankyul] (Add) UserRepository에 existsByStudentIdIn 추가

--- backend/src/main/java/OSSP/demo/repository/UserRepository.java
+++ backend/src/main/java/OSSP/demo/repository/UserRepository.java
@@ -4,6 +4,7 @@ import OSSP.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     /* 중복 검사> 중복인 경우 true, 중복되지 않은경우 false 리턴 */
     boolean existsByStudentId(String studentId);
+
+    boolean existsByStudentIdIn(List<String> members);
 }
