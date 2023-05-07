d3ccd65074308d96ccffb4459416ed1ca9386644
Hankyul Min <denirokr22@gmail.com>
2023-05-07 15:34:50 +0900

    [23.05.07, Hankyul] (Fix) config 오타 수정

--- backend/src/main/java/OSSP/demo/config/WebMvcConfig.java
+++ backend/src/main/java/OSSP/demo/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST");
     }
