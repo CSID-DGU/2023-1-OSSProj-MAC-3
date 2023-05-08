3c697aabf3bbc84e54a2f1509f92acdf3bd1a441
thisishwan2 <feel000617@gmail.com>
2023-05-08 15:54:50 +0900

    [23.05.08, pil hwan] (Fix) 맵핑 url 변경

--- backend/src/main/java/OSSP/demo/controller/FileUploadController.java
+++ backend/src/main/java/OSSP/demo/controller/FileUploadController.java
@@ -3,6 +3,7 @@ package OSSP.demo.controller;
 import OSSP.demo.model.FileVersionDto;
 import OSSP.demo.service.upload.FileUploadService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,8 +13,9 @@ public class FileUploadController {
 
     private final FileUploadService fileUploadService;
 
-    @PostMapping("/{memberId}/upload")
-    public String uploadFile(@PathVariable Long memberId, @RequestPart MultipartFile file, @RequestPart FileVersionDto fileVersionDto){
-        return fileUploadService.uploadImage(memberId, file, fileVersionDto);
+    @PostMapping("/{teamId}/upload")
+    public String uploadFile(@AuthenticationPrincipal String studentId, @PathVariable Long teamId, @RequestPart MultipartFile file,
+                             @RequestPart FileVersionDto fileVersionDto){
+        return fileUploadService.uploadImage(studentId, teamId, file, fileVersionDto);
     }
 }
