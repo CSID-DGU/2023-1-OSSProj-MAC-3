7240624cfc931b0000fd45ab323b1943838ca981
thisishwan2 <feel000617@gmail.com>
2023-05-09 23:46:25 +0900

    [23.05.09. pil hwan] (Mod) 응답처리 및 예외처리

--- backend/src/main/java/OSSP/demo/service/delete/FileDeleteService.java
+++ backend/src/main/java/OSSP/demo/service/delete/FileDeleteService.java
@@ -1,18 +1,24 @@
 package OSSP.demo.service.delete;
 
+import OSSP.demo.model.ResponseDto;
 import OSSP.demo.repository.FileRepository;
 import OSSP.demo.repository.FileVersionRepository;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class FileDeleteService {
 
     private final FileVersionRepository fileVersionRepository;
@@ -24,24 +30,34 @@ public class FileDeleteService {
 
     @Transactional
     // 파일 전체 삭제(하위 버전도 같이 삭제)
-    public String deleteAll(Long fileId){
-        String keyName = fileRepository.findById(fileId).get().getFileName(); //keyName이 파일 이름
-        String deleteUrl = fileRepository.findById(fileId).get().getS3FileUrl();
-        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, keyName));
-        fileRepository.deleteById(fileId); //db에서 삭제.
-        return deleteUrl;
+    public ResponseEntity deleteAll(Long fileId) {
+        try {
+            String keyName = fileRepository.findById(fileId).get().getFileName(); //keyName이 파일 이름
+            String deleteUrl = fileRepository.findById(fileId).get().getS3FileUrl();
+            amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, keyName));
+            fileRepository.deleteById(fileId); //db에서 삭제.
+            return ResponseEntity.ok().body(Collections.singletonMap("delete_file_result", "파일버전 전체 삭제를 성공했습니다."));
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(ResponseDto.builder().error(Collections.singletonMap("delete_file", "파일 전체 삭제에 실패했습니다.")));
+        }
     }
 
     @Transactional
     // 파일 버전 하나 삭제
-    public String deleteOne(Long fileVersionId){
-        String keyName = fileVersionRepository.findById(fileVersionId).get().getFile().getFileName();
-        String deleteUrl = fileVersionRepository.findById(fileVersionId).get().getS3FileVersionUrl();
+    public ResponseEntity deleteOne(Long fileVersionId){
+        try {
+            String keyName = fileVersionRepository.findById(fileVersionId).get().getFile().getFileName();
+            String deleteUrl = fileVersionRepository.findById(fileVersionId).get().getS3FileVersionUrl();
 
-        String versionId = deleteUrl.substring(deleteUrl.indexOf("=") + 1);
-        amazonS3Client.deleteVersion(bucket, keyName,versionId);
+            String versionId = deleteUrl.substring(deleteUrl.indexOf("=") + 1);
+            amazonS3Client.deleteVersion(bucket, keyName, versionId);
 
-        fileVersionRepository.deleteById(fileVersionId);
-        return deleteUrl;
+            fileVersionRepository.deleteById(fileVersionId);
+            return ResponseEntity.ok().body(Collections.singletonMap("delete_fileVersion_result", "한개의 파일 버전 삭제를 성공했습니다."));
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(ResponseDto.builder().error(Collections.singletonMap("delete_fileVersion", "파일 버전 삭제에 실패했습니다.")));
+        }
     }
 }
--- backend/src/main/java/OSSP/demo/service/upload/FileUploadService.java
+++ backend/src/main/java/OSSP/demo/service/upload/FileUploadService.java
@@ -1,10 +1,8 @@
 package OSSP.demo.service.upload;
 
-import OSSP.demo.entity.File;
-import OSSP.demo.entity.FileVersion;
-import OSSP.demo.entity.Member;
-import OSSP.demo.entity.User;
+import OSSP.demo.entity.*;
 import OSSP.demo.model.FileVersionDto;
+import OSSP.demo.model.ResponseDto;
 import OSSP.demo.repository.FileRepository;
 import OSSP.demo.repository.FileVersionRepository;
 import OSSP.demo.repository.MemberRepository;
@@ -14,12 +12,14 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -34,58 +34,72 @@ public class FileUploadService {
 
     //Multipart를 통해 전송된 파일을 업로드 하는 메소드
     @Transactional
-    public String uploadImage(String studentId, Long teamId, MultipartFile uploadFile,
-                              FileVersionDto fileVersionDto){
+    public ResponseEntity uploadImage(String studentId, Long teamId, MultipartFile uploadFile,
+                                      FileVersionDto fileVersionDto){
+        try {
+            User finduser = userRepository.findByStudentId(studentId).get();
 
-        User finduser = userRepository.findByStudentId(studentId).get();
-        System.out.println(finduser);
+            //현재 사용자 조회
+            Member member = memberRepository.findByUserIdAndTeamId(finduser.getId(), teamId).get();
 
-        //현재 사용자 조회
-        Member member = memberRepository.findByUserIdAndTeamId(finduser.getId(), teamId).get();
-        System.out.println(member);
-        // 파일 이름 추출
-        String fileName = uploadFile.getOriginalFilename();
+            // 파일 이름 추출
+            String fileName = uploadFile.getOriginalFilename();
 
-        // fileRepository에 현재 업로드 하는 파일과 같은 이름의 파일을 조회
-        File findfile = fileRepository.findFileByFileName(uploadFile.getOriginalFilename());
+            // fileRepository에 현재 업로드 하는 파일과 같은 이름의 파일을 조회
+            File findfile = fileRepository.findFileByFileName(uploadFile.getOriginalFilename());
 
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(uploadFile.getSize()); //파일 크기
-        objectMetadata.setContentType(uploadFile.getContentType()); // 파일 확장자
-        String versionId;
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(uploadFile.getSize()); //파일 크기
+            objectMetadata.setContentType(uploadFile.getContentType()); // 파일 확장자
+            String versionId;
 
-        try (InputStream inputStream = uploadFile.getInputStream()) {
-            //아래 라인을 통해 s3에 업로드 되는 파일의 Object 정보를 받아옴.
-            PutObjectResult putObjectResult = s3Service.uploadFile(inputStream, objectMetadata, fileName); // s3에 파일 업로드.
-            versionId = putObjectResult.getVersionId(); //버전id를 받아옴
-        } catch (IOException e) {
-            throw new IllegalArgumentException(String.format("파일 변환 중 에러가 발생하였습니다. (%s)", fileName));
-        }
+            try (InputStream inputStream = uploadFile.getInputStream()) {
+                //아래 라인을 통해 s3에 업로드 되는 파일의 Object 정보를 받아옴.
+                PutObjectResult putObjectResult = s3Service.uploadFile(inputStream, objectMetadata, fileName); // s3에 파일 업로드.
+                versionId = putObjectResult.getVersionId(); //버전id를 받아옴
+            } catch (IOException e) {
+                throw new IllegalArgumentException(String.format("파일 변환 중 에러가 발생하였습니다. (%s)", fileName));
+            }
 
-        String url = s3Service.getFileUrl(fileName); //업로드 파일 url 반환.
+            String url = s3Service.getFileUrl(fileName); //업로드 파일 url 반환.
 
-        // 파일이 없을시,
-        if (findfile == null) {
-            System.out.println(member);
-            //파일 객체 생성
-            File file = File.builder().
-                    fileName(fileName).
-                    member(member).
-                    s3FileUrl(url).
+            // 파일이 없을시,
+            if (findfile == null) {
+                System.out.println(member);
+                //파일 객체 생성
+                File file = File.builder().
+                        fileName(fileName).
+
+                        s3FileUrl(url).
+                        build();
+                fileRepository.save(file);
+                findfile = file;
+            }
+
+            findfile.setMember(member);
+
+            // FileVersion에 업로드한 파일을 저장.
+            FileVersion fileVersion = FileVersion.builder(). //컬럼으로 팀장이 합본을 올린것을 알 수 있도록 불리안으로 컬럼하나 생성.
+                    commitMessage(fileVersionDto.getCommitMessage()).
+                    combination(fileVersionDto.getCombination()).
+                    s3FileVersionUrl(url + "?versionId=" + versionId). //각 버전별 url
+//                            file(findfile).
                     build();
-            fileRepository.save(file);
-            findfile = file;
+
+            fileVersion.setFile(findfile);
+            fileVersion.setMember(member);
+
+            fileVersionRepository.save(fileVersion);
+
+            FileVersionDto fileVersionDto1 = FileVersionDto.builder().
+                    commitMessage(fileVersionDto.getCommitMessage()).
+                    combination(fileVersionDto.getCombination()).
+                    s3Url(url + "?versionId=" + versionId).build();
+
+            return ResponseEntity.ok().body(fileVersionDto1);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(ResponseDto.builder().error(Collections.singletonMap("upload_file", "파일 업로드에 실패했습니다.")).build());
         }
-
-        // FileVersion에 업로드한 파일을 저장.
-        FileVersion fileVersion = FileVersion.builder(). //컬럼으로 팀장이 합본을 올린것을 알 수 있도록 불리안으로 컬럼하나 생성.
-                commitMessage(fileVersionDto.getCommitMessage()).
-                combination(fileVersionDto.getCombination()).
-                s3FileVersionUrl(url + "?versionId=" + versionId). //각 버전별 url
-                        file(findfile).
-                build();
-
-        fileVersionRepository.save(fileVersion);
-        return url+"?versionId="+versionId;
     }
 }
