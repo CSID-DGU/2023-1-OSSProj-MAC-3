64e81793e7d9a70f4d2f22b0aa217c06d7d9238b
thisishwan2 <feel000617@gmail.com>
2023-05-09 23:46:50 +0900

    [23.05.09. pil hwan] (Mod) 응답처리

--- backend/src/main/java/OSSP/demo/controller/FileUploadController.java
+++ backend/src/main/java/OSSP/demo/controller/FileUploadController.java
@@ -3,10 +3,13 @@ package OSSP.demo.controller;
 import OSSP.demo.model.FileVersionDto;
 import OSSP.demo.service.upload.FileUploadService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class FileUploadController {
@@ -14,8 +17,8 @@ public class FileUploadController {
     private final FileUploadService fileUploadService;
 
     @PostMapping("/{teamId}/upload")
-    public String uploadFile(@AuthenticationPrincipal String studentId, @PathVariable Long teamId, @RequestPart MultipartFile file,
-                             @RequestPart FileVersionDto fileVersionDto){
+    public ResponseEntity uploadFile(@AuthenticationPrincipal String studentId, @PathVariable Long teamId, @RequestPart MultipartFile file,
+                                     @RequestPart FileVersionDto fileVersionDto){
         return fileUploadService.uploadImage(studentId, teamId, file, fileVersionDto);
     }
 }
