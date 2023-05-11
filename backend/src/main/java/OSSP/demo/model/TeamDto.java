68667315f89840dc6193323bcf1f91684e325e9d
Hankyul Min <denirokr22@gmail.com>
2023-05-12 08:22:23 +0900

    [23.05.12, Hankyul] (Mod) TeamDto 어노테이션 Data -> Getter

--- backend/src/main/java/OSSP/demo/model/TeamDto.java
+++ backend/src/main/java/OSSP/demo/model/TeamDto.java
@@ -1,16 +1,13 @@
 package OSSP.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
 public class TeamDto {
     private String teamName;
     private List<TeamFellowDto> teamFellow;
