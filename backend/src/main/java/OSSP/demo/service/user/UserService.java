4c48df5e6d39c38a601db074e5477b57f6bf55c6
Hankyul Min <denirokr22@gmail.com>
2023-05-20 20:56:27 +0900

    [23.05.20 Hankyul] (Add) UserService 에러 예외처리 추가

--- backend/src/main/java/OSSP/demo/service/user/UserService.java
+++ backend/src/main/java/OSSP/demo/service/user/UserService.java
@@ -18,34 +18,48 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-    public ResponseEntity getUser(String studentId) {
-        if(!userRepository.existsByStudentId(studentId)) {
-            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("get_user", "사용자가 존재하지 않습니다")).build();
-            return ResponseEntity.badRequest().body(responseErrorDto);
-        }
-        User user = userRepository.findByStudentId(studentId).get();
-        UserDto userDto = UserDto.builder()
-                .id(user.getId())
-                .studentId(user.getStudentId())
-                .name(user.getName())
-                .dept(user.getDept())
-                .build();
-        return ResponseEntity.ok(userDto);
-    }
 
-    public ResponseEntity getUserList(String studentId) {
-        List<User> users = userRepository.findAll();
-        users.remove(userRepository.findByStudentId(studentId).get());
-        List<UserDto> userDtoList = new ArrayList<>();
-        for (User user : users) {
+    public ResponseEntity getUser(String studentId) {
+        try {
+            if (!userRepository.existsByStudentId(studentId)) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("get_user", "사용자가 존재하지 않습니다")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
+            }
+            User user = userRepository.findByStudentId(studentId).get();
             UserDto userDto = UserDto.builder()
                     .id(user.getId())
                     .studentId(user.getStudentId())
                     .name(user.getName())
                     .dept(user.getDept())
                     .build();
-            userDtoList.add(userDto);
+            return ResponseEntity.ok(userDto);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("get_user", "사용자를 불러오는데 실패했습니다")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
-        return ResponseEntity.ok(Collections.singletonMap("get_user_list", userDtoList));
+
+    }
+
+    public ResponseEntity getUserList(String studentId) {
+        try {
+            List<User> users = userRepository.findAll();
+            users.remove(userRepository.findByStudentId(studentId).get());
+            List<UserDto> userDtoList = new ArrayList<>();
+            for (User user : users) {
+                UserDto userDto = UserDto.builder()
+                        .id(user.getId())
+                        .studentId(user.getStudentId())
+                        .name(user.getName())
+                        .dept(user.getDept())
+                        .build();
+                userDtoList.add(userDto);
+            }
+            return ResponseEntity.ok(Collections.singletonMap("get_user_list", userDtoList));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("get_user_list", "사용자 목록을 불러오는데 실패했습니다")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
+        }
     }
 }
