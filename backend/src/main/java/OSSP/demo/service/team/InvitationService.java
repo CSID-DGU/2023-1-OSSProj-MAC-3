f9a83415f47870a22c59dd4152ec3be036c849bc
Hankyul Min <denirokr22@gmail.com>
2023-05-20 20:54:58 +0900

    [23.05.20 Hankyul] (Mod) InvitationService 응답바디 개선, 에러 처리 개선

--- backend/src/main/java/OSSP/demo/service/team/InvitationService.java
+++ backend/src/main/java/OSSP/demo/service/team/InvitationService.java
@@ -4,6 +4,7 @@ import OSSP.demo.entity.Invitation;
 import OSSP.demo.entity.Member;
 import OSSP.demo.entity.Role;
 import OSSP.demo.model.InvitationDto;
+import OSSP.demo.model.ResponseDto;
 import OSSP.demo.repository.InvitationRepository;
 import OSSP.demo.repository.MemberRepository;
 import OSSP.demo.repository.TeamRepository;
@@ -30,48 +31,63 @@ public class InvitationService {
     MemberRepository memberRepository;
 
     public ResponseEntity sendInvitations(String studentId, InvitationDto.InvitationRequestDto invitationDto) {
-        if (!userRepository.existsByStudentId(studentId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("send_invitation", "사용자 정보가 존재하지 않습니다."));
-        }
-        Long leaderId = userRepository.findByStudentId(studentId).get().getId();
-        if (!memberRepository.existsByUserIdAndTeamId(leaderId, invitationDto.getTeamId())) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("send_invitation", "팀장이 아닙니다."));
-        }
-        if (invitationDto.getFellowIds().isEmpty()) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("send_invitation", "초대할 사용자가 존재하지 않습니다."));
-        }
-        List<InvitationDto> invitationDtoList = new ArrayList<>();
-        for (Long fellowId : invitationDto.getFellowIds()) {
-            if (memberRepository.existsByUserIdAndTeamId(fellowId, invitationDto.getTeamId())) {
-                return ResponseEntity.badRequest().body(Collections.singletonMap("send_invitation", "이미 팀에 가입된 사용자가 포함되어 있습니다."));
+        try {
+            if (!userRepository.existsByStudentId(studentId)) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("send_invitation", "사용자 정보가 존재하지 않습니다.")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
             }
-            if (invitationRepository.existsByLeaderIdAndFellowIdAndTeamId(leaderId, fellowId, invitationDto.getTeamId())) {
-                return ResponseEntity.badRequest().body(Collections.singletonMap("send_invitation", "이미 초대된 사용자가 포함되어 있습니다."));
+            Long leaderId = userRepository.findByStudentId(studentId).get().getId();
+            if (!memberRepository.existsByUserIdAndTeamId(leaderId, invitationDto.getTeamId())) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("send_invitation", "팀장이 아닙니다.")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
             }
-            Invitation invitation = Invitation.builder()
-                    .leader(userRepository.findById(leaderId).get())
-                    .fellow(userRepository.findById(fellowId).get())
-                    .team(teamRepository.findById(invitationDto.getTeamId()).get())
-                    .build();
-            invitationRepository.save(invitation);
-            InvitationDto invitationResponseDto = InvitationDto.builder()
-                    .id(invitation.getId())
-                    .leaderId(invitation.getLeader().getId())
-                    .fellowId(invitation.getFellow().getId())
-                    .teamId(invitation.getTeam().getId())
-                    .build();
-            invitationDtoList.add(invitationResponseDto);
+            if (invitationDto.getFellowIds().isEmpty()) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("send_invitation", "초대할 사용자가 존재하지 않습니다.")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
+            }
+            List<InvitationDto> invitationDtoList = new ArrayList<>();
+            for (Long fellowId : invitationDto.getFellowIds()) {
+                if (memberRepository.existsByUserIdAndTeamId(fellowId, invitationDto.getTeamId())) {
+                    ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("send_invitation", "이미 팀에 가입된 사용자가 포함되어 있습니다.")).build();
+                    return ResponseEntity.badRequest().body(responseErrorDto);
+                }
+                if (invitationRepository.existsByLeaderIdAndFellowIdAndTeamId(leaderId, fellowId, invitationDto.getTeamId())) {
+                    ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("send_invitation", "이미 초대된 사용자가 포함되어 있습니다.")).build();
+                    return ResponseEntity.badRequest().body(responseErrorDto);
+                }
+                Invitation invitation = Invitation.builder()
+                        .leader(userRepository.findById(leaderId).get())
+                        .fellow(userRepository.findById(fellowId).get())
+                        .team(teamRepository.findById(invitationDto.getTeamId()).get())
+                        .build();
+                invitationRepository.save(invitation);
+                InvitationDto invitationResponseDto = InvitationDto.builder()
+                        .id(invitation.getId())
+                        .leaderId(invitation.getLeader().getId())
+                        .fellowId(invitation.getFellow().getId())
+                        .teamId(invitation.getTeam().getId())
+                        .build();
+                invitationDtoList.add(invitationResponseDto);
+            }
+            return ResponseEntity.ok().body(Collections.singletonMap("send_invitations", invitationDtoList));
         }
-        return ResponseEntity.ok().body(Collections.singletonMap("send_invitations", invitationDtoList));
+        catch (Exception e) {
+            log.error(e.getMessage());
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("send_invitation", "초대에 실패하였습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
+        }
+
     }
 
     public ResponseEntity getInvitations(String studentId) {
         if (!userRepository.existsByStudentId(studentId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("get_invitations", "사용자 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("get_invitations", "사용자 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Long fellowId = userRepository.findByStudentId(studentId).get().getId();
         if (!invitationRepository.existsByFellowId(fellowId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("get_invitations", "초대된 팀이 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("get_invitations", "초대된 팀이 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         List<Invitation> invitations = invitationRepository.findAllByFellowId(fellowId);
         List<InvitationDto> invitationDtoList = new ArrayList<>();
@@ -89,18 +105,22 @@ public class InvitationService {
 
     public ResponseEntity acceptInvitation(String studentId, Long invitationId) {
         if (!userRepository.existsByStudentId(studentId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("accept_invitation", "사용자 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("accept_invitation", "사용자 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Long memberId = userRepository.findByStudentId(studentId).get().getId();
         if (!invitationRepository.existsById(invitationId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("accept_invitation", "초대 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("accept_invitation", "초대 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Invitation invitation = invitationRepository.findById(invitationId).get();
         if (invitation.getFellow().getId() != memberId) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("accept_invitation", "초대된 사용자가 아닙니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("accept_invitation", "초대된 사용자가 아닙니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         if (memberRepository.existsByUserIdAndTeamId(memberId, invitation.getTeam().getId())) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("accept_invitation", "이미 팀에 가입된 사용자입니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("accept_invitation", "이미 팀에 가입된 사용자입니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         invitation.setIsAccepted(true);
         Member member = new Member(invitation.getFellow(), invitation.getTeam(), Role.Member);
@@ -112,24 +132,28 @@ public class InvitationService {
                 .teamId(invitation.getTeam().getId())
                 .isAccepted(invitation.getIsAccepted())
                 .build();
-        return ResponseEntity.ok().body(Collections.singletonMap("accept_invitation", invitationResponseDto));
+        return ResponseEntity.ok().body(invitationResponseDto);
     }
 
 
     public ResponseEntity rejectInvitation(String studentId, Long invitationId) {
         if (!userRepository.existsByStudentId(studentId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("reject_invitation", "사용자 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("reject_invitation", "사용자 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Long memberId = userRepository.findByStudentId(studentId).get().getId();
         if (!invitationRepository.existsById(invitationId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("reject_invitation", "초대 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("reject_invitation", "초대 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Invitation invitation = invitationRepository.findById(invitationId).get();
         if (invitation.getFellow().getId() != memberId) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("reject_invitation", "초대된 사용자가 아닙니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("reject_invitation", "초대된 사용자가 아닙니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         if (invitation.getIsAccepted()) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("reject_invitation", "이미 수락된 초대입니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("reject_invitation", "이미 수락된 초대입니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         invitation.setIsAccepted(false);
         InvitationDto invitationResponseDto = InvitationDto.builder()
@@ -139,20 +163,23 @@ public class InvitationService {
                 .teamId(invitation.getTeam().getId())
                 .isAccepted(invitation.getIsAccepted())
                 .build();
-        return ResponseEntity.ok().body(Collections.singletonMap("reject_invitation", invitationResponseDto));
+        return ResponseEntity.ok().body(invitationResponseDto);
     }
 
     public ResponseEntity deleteInvitation(String studentId, Long invitationId) {
         if (!userRepository.existsByStudentId(studentId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("delete_invitation", "사용자 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("delete_invitation", "사용자 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Long leaderId = userRepository.findByStudentId(studentId).get().getId();
         if (!invitationRepository.existsById(invitationId)) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("delete_invitation", "초대 정보가 존재하지 않습니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("delete_invitation", "초대 정보가 존재하지 않습니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         Invitation invitation = invitationRepository.findById(invitationId).get();
         if (invitation.getLeader().getId() != leaderId) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("delete_invitation", "초대한 사용자가 아닙니다."));
+            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("delete_invitation", "초대한 사용자가 아닙니다.")).build();
+            return ResponseEntity.badRequest().body(responseErrorDto);
         }
         invitationRepository.deleteById(invitationId);
         return ResponseEntity.ok().body(Collections.singletonMap("delete_invitation", "초대가 삭제되었습니다."));
