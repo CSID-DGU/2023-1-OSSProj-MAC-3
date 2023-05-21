c2d641255c90bdea586787677d0f1ea385d717fd
Hankyul Min <denirokr22@gmail.com>
2023-05-21 20:23:07 +0900

    [23.05.21 Hankyul] (Add) 초대 fetch 초대장 수 표시

--- frontend/startbootstrap-sb-admin-2-gh-pages/demo/src/components/InvitationNav.js
+++ frontend/startbootstrap-sb-admin-2-gh-pages/demo/src/components/InvitationNav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
 import InviteMsg from "./InvitationMsg";
@@ -6,6 +6,26 @@ import DropdownButton from "./DropdownButton";
 
 const InvitationNav = () => {
   const [alertCount, setAlertCount] = useState(0);
+
+  const fetchInvitation = () => {
+    const token = sessionStorage.getItem("token");
+    fetch("http://localhost:8080/team/invitation", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && Array.isArray(data.get_invitations)) {
+          setAlertCount(data.get_invitations.length);
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchInvitation();
+  }, []);
 
   return (
     <li className="nav-item dropdown no-arrow mx-1">
@@ -15,7 +35,7 @@ const InvitationNav = () => {
             <FontAwesomeIcon icon={faMessage} />
             {/* Counter - Alerts */}
             <span className="badge badge-danger badge-counter">
-              {alertCount > 0 ? alertCount : "3+"}
+              {alertCount > 0 ? alertCount : 0}
             </span>
           </div>
         }
