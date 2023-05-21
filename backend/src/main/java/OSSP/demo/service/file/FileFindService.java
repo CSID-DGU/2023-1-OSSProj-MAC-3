96cd912fcecaa22f8e94ef6621cd321eeede3ea7
thisishwan2 <feel000617@gmail.com>
2023-05-21 12:35:14 +0900

    [23.05.21, pil hwan](Mod) 페이징 로직 추가

--- backend/src/main/java/OSSP/demo/service/file/FileFindService.java
+++ backend/src/main/java/OSSP/demo/service/file/FileFindService.java
@@ -1,15 +1,15 @@
 package OSSP.demo.service.file;
 
-import OSSP.demo.entity.File;
 import OSSP.demo.model.FilePagingResponseDto;
 import OSSP.demo.repository.FileRepository;
+import OSSP.demo.repository.FileVersionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @Slf4j
@@ -18,9 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class FileFindService {
 
     private final FileRepository fileRepository;
+    private final FileVersionRepository fileVersionRepository;
 
-    public Page<FilePagingResponseDto> findAllFile(Pageable pageable, Long teamId){
+    public Page<FilePagingResponseDto> findAllFile(Pageable pageable, String studentId, Long teamId){
         return fileRepository.findByTeamIdOrderByUpdateDateDesc(teamId, pageable)
+                .map(FilePagingResponseDto::from);
+    }
+
+    public Page<FilePagingResponseDto> findAllFileVersion(Pageable pageable, String studentId, Long teamId, Long fileId){
+        return fileVersionRepository.findByFile_FileIdOrderByCreatedDateDesc(fileId, pageable)
                 .map(FilePagingResponseDto::from);
     }
 }
