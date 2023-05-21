46ab689133302fe5b7a324eba93178d5b89a272b
thisishwan2 <feel000617@gmail.com>
2023-05-21 12:32:18 +0900

    [23.05.21, pil hwan](Mod) Paging 메서드 추가

--- backend/src/main/java/OSSP/demo/repository/FileVersionRepository.java
+++ backend/src/main/java/OSSP/demo/repository/FileVersionRepository.java
@@ -1,9 +1,15 @@
 package OSSP.demo.repository;
 
 import OSSP.demo.entity.FileVersion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileVersionRepository extends JpaRepository<FileVersion, Long> {
 
     FileVersion findByFileVersionIdAndFile_FileId(Long fileId, Long fileVersionId);
+
+    Page<FileVersion> findByFile_FileIdOrderByCreatedDateDesc(Long fileId, Pageable pageable);
 }
