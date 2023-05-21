b8144734fff67af5762b5e993283effb39f8d363
Hankyul Min <denirokr22@gmail.com>
2023-05-21 19:40:41 +0900

    [23.05.21 Hankyul] (Add) TeamInfo.js Api 패치, 모달

--- frontend/startbootstrap-sb-admin-2-gh-pages/demo/src/components/TeamInfo.js
+++ frontend/startbootstrap-sb-admin-2-gh-pages/demo/src/components/TeamInfo.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-const TeamInfo = () => {
+const TeamInfo = ({ teamId, handleInviteModalShow }) => {
+  const [teamInfo, setTeamInfo] = useState(null);
+
+  const fetchTeam = () => {
+    const token = sessionStorage.getItem("token");
+    fetch("http://localhost:8080/team/" + teamId.id, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTeamInfo(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    if (teamId.id === 0) return;
+    fetchTeam();
+  }, [teamId]);
+
   return (
     <div>
       {/* 팀 구성 정보 */}
@@ -12,38 +33,53 @@ const TeamInfo = () => {
           <div className="dropdown no-arrow">
             <a
               className="dropdown-toggle"
-              href="#"
               role="button"
               id="dropdownMenuLink"
               data-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
+              onClick={() => {
+                if (teamId.id > 0) {
+                  handleInviteModalShow(true);
+                } else {
+                  window.alert("팀을 선택해주세요.");
+                }
+              }}
             >
               <FontAwesomeIcon icon={faPlus} />
             </a>
-            <div
-              className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-              aria-labelledby="dropdownMenuLink"
-            >
-              <div className="dropdown-header">Dropdown Header:</div>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
           </div>
         </div>
         <div className="card-body">
-          <ul className="list-group">
-            <li className="list-group-item">20230001 김동국</li>
-            <li className="list-group-item">20230002 김건국</li>
-          </ul>
+          {teamInfo ? (
+            <div
+              className="table-responsive project-list"
+              style={{ maxHeight: "150px", overflowY: "auto" }}
+            >
+              <table className="table project-table table-centered table-nowrap">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">학번</th>
+                    <th scope="col">이름</th>
+                    <th scope="col">역할</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {teamInfo.teamFellow.map((fellow, index) => (
+                    <tr key={index}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{fellow.studentId}</td>
+                      <td>{fellow.name}</td>
+                      <td>{fellow.role === "Leader" ? "팀장" : "팀원"}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
     </div>
