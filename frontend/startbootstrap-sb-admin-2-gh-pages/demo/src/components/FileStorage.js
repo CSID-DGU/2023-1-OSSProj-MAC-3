952509ad0232ba60ff5dfc14669aaadc9bbbdeb8
ahnup <sy990607@naver.com>
2023-05-26 20:46:54 +0900

    [23.05.26. SangYeon] (Mod) FileStorage.js 파일리스트 출력

--- frontend/startbootstrap-sb-admin-2-gh-pages/demo/src/components/FileStorage.js
+++ frontend/startbootstrap-sb-admin-2-gh-pages/demo/src/components/FileStorage.js
@@ -10,11 +10,34 @@ import {
 
 const FileStorage = ({
   teamId,
-  handleUploadModalShow,
   handleHistoryModalShow,
+  handleUploadModalShow,
 }) => {
   const [fileList, setFileList] = useState(null);
+  const [fileId, setFileId] = useState(0);
 
+  useEffect(() => {
+    console.log(fileId);
+  }, [fileId]);
+
+  const fetchData = () => {
+    const token = sessionStorage.getItem("token");
+    fetch(`http://localhost:8080/team/${teamId.id}/file`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setFileList(data.get_files);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    if (teamId.id === 0) return;
+    fetchData();
+  }, [teamId]);
   return (
     <div className="col-xl-8">
       <div className="card shadow mb-4">
@@ -74,137 +97,60 @@ const FileStorage = ({
                           </tr>
                         </thead>
                         <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>OSS 착수</td>
-                            <td>서론 작성</td>
-                            <td>02/5/2019</td>
-                            <td>김동국</td>
-                            <td style={{ textAlign: "center" }}>
-                              <a
-                                className="btn"
-                                style={{ padding: "0.1rem 0.5rem" }}
-                              >
-                                <FontAwesomeIcon icon={faEdit} />
-                              </a>
-                              <a
-                                className="btn"
-                                style={{ padding: "0.1rem 0.5rem" }}
-                              >
-                                <FontAwesomeIcon icon={faTrash} />
-                              </a>
-                              <a
-                                className="btn"
-                                style={{ padding: "0.1rem 0.5rem" }}
-                              >
-                                <FontAwesomeIcon icon={faDownload} />
-                              </a>
-                              <div
-                                className="dropdown no-arrow btn"
-                                style={{
-                                  display: "inline",
-                                  padding: "0.1rem 0.5rem",
-                                }}
-                              >
-                                <a
-                                  className="dropdown-toggle"
-                                  href="#"
-                                  role="button"
-                                  onClick={() => {
-                                    if (teamId.id > 0) {
-                                      handleHistoryModalShow(true);
-                                    } else {
-                                      window.alert("팀을 선택해주세요.");
-                                    }
-                                  }}
-                                >
-                                  <FontAwesomeIcon icon={faBars} />
-                                </a>
-                                <div
-                                  className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                  aria-labelledby="dropdownMenuLink"
-                                >
-                                  <div className="dropdown-header">
-                                    Dropdown Header:
+                          {fileList &&
+                            fileList.map((file, index) => (
+                              <tr key={file.fileId}>
+                                <th scope="row">{file.fileId}</th>
+                                <td>{file.fileName}</td>
+                                <td>{file.commitMessage}</td>
+                                <td>{file.updateDate}</td>
+                                <td>{file.memberName}</td>
+                                <td style={{ textAlign: "center" }}>
+                                  {/* Action buttons */}
+                                </td>
+                                <td style={{ textAlign: "center" }}>
+                                  <a
+                                    className="btn"
+                                    style={{ padding: "0.1rem 0.5rem" }}
+                                  >
+                                    <FontAwesomeIcon icon={faEdit} />
+                                  </a>
+                                  <a
+                                    className="btn"
+                                    style={{ padding: "0.1rem 0.5rem" }}
+                                  >
+                                    <FontAwesomeIcon icon={faTrash} />
+                                  </a>
+                                  <a
+                                    className="btn"
+                                    style={{ padding: "0.1rem 0.5rem" }}
+                                  >
+                                    <FontAwesomeIcon icon={faDownload} />
+                                  </a>
+                                  <div
+                                    className="dropdown no-arrow btn"
+                                    style={{
+                                      display: "inline",
+                                      padding: "0.1rem 0.5rem",
+                                    }}
+                                  >
+                                    <a
+                                      className="dropdown-toggle"
+                                      role="button"
+                                      onClick={() => {
+                                        if (teamId.id > 0) {
+                                          handleHistoryModalShow(true);
+                                        } else {
+                                          window.alert("팀을 선택해주세요.");
+                                        }
+                                      }}
+                                    >
+                                      <FontAwesomeIcon icon={faBars} />
+                                    </a>
                                   </div>
-                                  <a className="dropdown-item" href="#">
-                                    Action
-                                  </a>
-                                  <a className="dropdown-item" href="#">
-                                    Another action
-                                  </a>
-                                  <div className="dropdown-divider"></div>
-                                  <a className="dropdown-item" href="#">
-                                    Something else here
-                                  </a>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>OSS 착수</td>
-                            <td>서론 작성</td>
-                            <td>02/5/2019</td>
-                            <td>김동국</td>
-                            <td style={{ textAlign: "center" }}>
-                              <a
-                                className="btn"
-                                style={{ padding: "0.1rem 0.5rem" }}
-                              >
-                                <FontAwesomeIcon icon={faEdit} />
-                              </a>
-                              <a
-                                className="btn"
-                                style={{ padding: "0.1rem 0.5rem" }}
-                              >
-                                <FontAwesomeIcon icon={faTrash} />
-                              </a>
-                              <a
-                                className="btn"
-                                style={{ padding: "0.1rem 0.5rem" }}
-                              >
-                                <FontAwesomeIcon icon={faDownload} />
-                              </a>
-                              <div
-                                className="dropdown no-arrow btn"
-                                style={{
-                                  display: "inline",
-                                  padding: "0.1rem 0.5rem",
-                                }}
-                              >
-                                <a
-                                  className="dropdown-toggle"
-                                  href="#"
-                                  role="button"
-                                  id="dropdownMenuLink"
-                                  data-toggle="dropdown"
-                                  aria-haspopup="true"
-                                  aria-expanded="false"
-                                >
-                                  <FontAwesomeIcon icon={faBars} />
-                                </a>
-                                <div
-                                  className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                  aria-labelledby="dropdownMenuLink"
-                                >
-                                  <div className="dropdown-header">
-                                    Dropdown Header:
-                                  </div>
-                                  <a className="dropdown-item" href="#">
-                                    Action
-                                  </a>
-                                  <a className="dropdown-item" href="#">
-                                    Another action
-                                  </a>
-                                  <div className="dropdown-divider"></div>
-                                  <a className="dropdown-item" href="#">
-                                    Something else here
-                                  </a>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
+                                </td>
+                              </tr>
+                            ))}
                         </tbody>
                       </table>
                     </div>
